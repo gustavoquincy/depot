51a0ee0477ebede0dee8d03abbc6efc1a04b5220
kaileyuan <kaileyuan@protonmail.com>
2020-08-24 16:32:23 -0500

    Connecting products to carts

---
+++ app/javascript/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-right>Toggle Sidebar</b-button>
+    <b-sidebar id="sidebar-1" title="Sidebar" shadow="">
+      <div class="px-3 py-2">
+        <p>
+          testing.testing
+        </p>
+        <b-img src="app/assets/images/mug.jpg" snowpeak mug></b-img>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!",
+      status: "not_accepted"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
